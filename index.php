cac631d0a0490610c5a36ed1665781831f408239
Iker Ocio <iker@ikerocio.com>
2017-05-07 18:29:54 +0200

    CV Updated

--- index.php
+++ index.php
@@ -17,17 +17,69 @@
         <!-- Font Awesome -->
         <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
         <!-- Custom styles for this template -->
-        <link href="css/jumbotron.css" rel="stylesheet">
+        <!--<link href="css/jumbotron.css" rel="stylesheet">-->
 
-        <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-        <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-        <script src="js/ie-emulation-modes-warning.js"></script>
+
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!--[if lt IE 9]>
           <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
           <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
+        <style>
+            body{
+                margin-top:10px;
+            }
+
+            .col-xs-12> i.fa{
+                float:left;
+                margin:5px;
+            }
+
+            p.skill-title{
+                font-weight: bold;
+                margin-top:8px;
+            }
+
+            .css-item{
+                /* Firefox */
+                -moz-transition: all 1s ease;
+                /* WebKit */
+                -webkit-transition: all 1s ease;
+                /* Opera */
+                -o-transition: all 1s ease;
+                /* Standard */
+                transition: all 1s ease;
+            }
+
+            .css-item:hover{
+                /* Firefox */
+                -moz-transform: scale(1.5) rotate(15deg) translate(50px);
+                /* WebKit */
+                -webkit-transform: scale(1.5) rotate(15deg) translate(50px);
+                /* Opera */
+                -o-transform: scale(1.5) rotate(15deg) translate(50px);
+                /* Standard */
+                transform: scale(1.5) rotate(-15deg) translate(50px);
+
+                z-index: 1000;
+            }
+
+            a{
+                font-weight:bold;
+                text-decoration: none;
+            }
+
+            a:hover{
+                text-decoration: none;
+
+            }
+            .bold{
+                font-weight:bold;
+            }
+
+
+        </style>
     </head>
 
     <body>
@@ -49,7 +101,7 @@
                 </div>
             </div>
 
-                <!-- Example row of columns -->
+                <!-- row of columns -->
                 <div class="row">
                     <div class="col-xs-12 col-lg-3">
                         <i class="fa fa-cube fa-2x"></i>
@@ -58,26 +110,26 @@
                     </div>
                     <div class="col-xs-12 col-lg-3">
                         <i class="fa fa-cubes fa-2x"></i>
-                        <p class="skill-title">Django</p>
-                        <p>Y no precisamente la pelicula. Este framework de Python es capaz de crear sistemas de blog muy complejos en pocas horas (o minutos).</p>
+                        <p class="skill-title">Linux</p>
+                        <p>Porque hay vida más allá de Windows. Cuando aprendes la potencia de hacer las cosas a golpe de comando y lo complicado que es mediante interfaz... Te planteas valorar el lado oscuro.</p>
                     </div>
                     <div class="col-xs-12 col-lg-3">
                         <i class="fa fa-arrows fa-2x"></i>
                         <p class="skill-title">Javascript</p>
-                        <p>Hablar de desarrollo web en el siglo XXI sin nombrar Javascript o una de sus librerías es imposible. Y la tendencia no vaticina que esto vaya a cambiar.</p>
+                        <p>O VanillaJS, como prefieras. Hablar del Frontend en el siglo XXI sin nombrar Javascript o una de sus muchas librerías es imposible. Y no parece que vaya a cambiar. Por tanto... indispensable.</p>
                     </div>
-                    <div class="col-xs-12 col-lg-3">
+                    <div class="col-xs-12 col-lg-3 css-item">
                         <i class="fa fa-tags fa-2x"></i>
-                        <p class="skill-title">HTML</p>
-                        <p>No sabría si llamarlo lenguaje de programación, pero aún recuerdo la emoción que sentí cuando empecé con el mundo de las etiquetas. No era consciente de todo lo que me quedaba por aprender.</p>
+                        <p class="skill-title">CSS</p>
+                        <p>Digan lo que digan. Las cosas siempre quedan más bonitas cuando las maquillas. Y el desarrollo web no iba a ser menos.</p>
                     </div>
                 </div>
-                <!-- Example row of columns -->
+                <!-- row of columns -->
                 <div class="row">
                     <div class="col-xs-12 col-lg-3">
                         <i class="fa fa-coffee fa-2x"></i>
                         <p class="skill-title">Java</p>
-                        <p>Java y sus interminables líneas de código para hacer cosas tan simples como abrir un fichero. Nos guste o no, tenemos que aprobar la aceptación de este lenguaje en el día a día.</p>
+                        <p>La potencia de la multiplataforma y su mercado. Hoy en día nos rodean infinidad de aplicaciones que derivan de este lenguaje. Además, fue con el que aprendí a programar de verdad.</p>
                     </div>
 
                     <div class="col-xs-12 col-lg-3">
@@ -88,32 +140,28 @@
                     <div class="col-xs-12 col-lg-3">
                         <i class="fa fa-cogs fa-2x"></i>
                         <p class="skill-title">MySQL</p>
-                        <p>Por que los datos no siempre se pueden almacenar en ficheros de configuracion. Las bases de datos tambien conviven entre nosotros. </p>
+                        <p>Por que a veces los datos no podemos guardarlos en ficheros de texto plano. Las bases de datos tambien conviven entre nosotros. </p>
                     </div>
                     <div class="col-xs-12 col-lg-3">
                         <i class="fa fa-android fa-2x"></i>
                         <p class="skill-title">Android</p>
-                        <p>Poco tiempo entre nosotros pero lo suficiente para jugar un poco con el. Android (y otros SO moviles) son el futuro y hay que aprender de/con ellos. </p>
+                        <p>Es innegable el auge que tienen los SO móviles hoy en día. Si no quieres convertirte en un dinosaurio, debes aprender algo sobre ello. </p>
                     </div>
                 </div> 
                 <hr>
                 <div class="row">
                     <div class="col-xs-12 col-lg-6">
-                        <i class="fa fa-code-fork fa-2x"></i>
-                        <p class="skill-title">Proyectos Open Source</p>
+                        <i class="fa fa-check-square-o fa-2x"></i>
+                        <p class="skill-title">Experiencia</p>
                         <ul>
-                            <li><a href="https://github.com/iocio005/android-loterianavidad/"><p class="skill-sub">Loteria Navidad</p></a>Aplicación basada en Android para comprobar los números del sorteo de la Loteria de la Navidad. Publicada en <a href="https://play.google.com/store/apps/details?id=com.ocioz.loterianavidad">Google Play.</a></li>
-                            <li><a href="https://github.com/iocio005/python-easymail/"><p class="skill-sub">Python-EasyMail</p></a>Wrapper entorno a las librerias smtplib y email.mime de Python para un uso sencillo de las mismas.</li>
-                            <li><a href="https://github.com/iocio005/python-dbuniverse-downloader"><p class="skill-sub">Python-Scrapping</p></a>Un poco de juego con Python y web scrapping.</li>
-                        </ul>
-                        <p class="sub">Otros</p>
-                        <ul>
-                            <li><a href="">Github</a></li>
+                            <li><span class="bg bold">Freelance</span> <a class="bold" href="#">2015-</a><br> Desde entonces trabajo como autonomo con pequeños proyectos que consigo abordar en mi tiempo libre.</li>
+                            <li><span class="bg bold">Njoy Party & Events</span> <a class="italic" href="http://n-joyparty.com/">2014-2015</a><br> Desarrollador de Software, Webmaster y SysAdmin para ésta pequeña empresa de eventos. Software a medida para hacer del ocio algo aun más divertido.</li>
+                            <li><span class="bg bold">Media Markt Vitoria</span> <a class="italic" href="http://www.mediamarkt.es/">2010</a><br> Vendedor en departamento de informática en MM Vitoria.</li>
                         </ul>
                     </div>
                     <div class="col-xs-12 col-lg-6">
                         <i class="fa fa-users fa-2x"></i>
-                        <p class="skill-title">Conferencias / Charlas</p>
+                        <p class="skill-title">Asistencia a Conferencias / Charlas</p>
                         <ul>
                             <li><span class="bg">08/11/2014:</span><a href="http://2014.es.pycon.org/">PyCon Zaragoza</a></li>
                             <li><span class="bg">23/11/2013:</span><a href="http://2013.es.pycon.org/">PyCon Madrid</a></li>
@@ -124,12 +172,15 @@
                 <hr>
                 <div class="row">
                     <div class="col-xs-12 col-lg-6">
-                        <i class="fa fa-check-square-o fa-2x"></i>
-                        <p class="skill-title">Experiencia</p>
+                        <i class="fa fa-certificate fa-2x"></i>
+                        <p class="skill-title">Diplomas</p>
                         <ul>
-                            <li><span class="bg">Njoy Party & Events</span> <a class="italic" href="http://n-joyparty.com/">2014-2015</a><br> Desarrollador de Software, Webmaster y SysAdmin para ésta pequeña empresa de eventos. Software a medida para hacer del ocio algo aun más divertido.</li>
-                            <li><span class="bg">Peluquería H2U</span> <a class="italic" href="http://www.peluqueriah2u.com">2014</a><br> Agenda de citas a medida para centro de estética y belleza.</li>
-                            <li><span class="bg">MediaMarkt Vitoria</span> <a class="italic" href="http://www.mediamarkt.es/">2010</a><br> Vendedor en departamento de informática en MM Vitoria.</li>
+                            <li><a class="italic" href="http://www.spri.eus/euskadinnova/es/enpresa-digitala/agenda/desarrollo-agil-con-python-django/3889.aspx"><span class="bg">Desarrollo Web ágil con Python y Django (15h)</span></a></li>
+                            <li><a class="italic" href="http://www.cisco.com/c/en/us/training-events/training-certifications/certifications/associate/ccna-routing-switching.html"><span class="bg">Cisco CCNA2: Routing and Switching essentials</span> </a></li>
+                            <li><a class="italic" href="http://www.cisco.com/c/en/us/training-events/training-certifications/certifications/associate/ccna-routing-switching.html"><span class="bg">Cisco CCNA3: Scaling Networks</span></a></li>
+                            <li><a class="italic" href="http://www.cisco.com/c/en/us/training-events/training-certifications/certifications/associate/ccna-routing-switching.html"><span class="bg">Cisco CCNA4: Routing and Switching. Connecting Networks</span></a></li>
+                            <li><a class="italic" href="https://learningnetwork.cisco.com/community/certifications/iot"><span class="bg">Cisco IoE: Introduction to the Internet of Everything</span></a></li>
+
                         </ul>
                     </div>
                     <div class="col-xs-12 col-lg-6">
@@ -139,11 +190,8 @@
                             <li><span class="bg">2010-Actualidad:</span> <a class="italic" href="http://www.mondragon.edu/es/es">Universidad de Mondragón</a><br> Ingeniería Informática</li>
                             <li><span class="bg">2008-2010:</span> <a class="italic">Colegio Urkide</a><br> Bachillerato Cientifico Técnico</li>
                         </ul>
-                        <p class="sub">Otros</p>
-                        <ul>
-                            <li><a href="http://django.es/formacion/desarrollo-web-agil-python-django2/">Desarrollo Web Agil con Django</a></li>
-                        </ul>
-                        <p class="sub">Sobre mí</p>
+                        <!--<i class="fa fa-user fa-2x"></i>-->
+                        <p class="sub bold">Sobre mí</p>
                         <ul>
                             <li><p>Soy una persona bastante competitiva en constante aprendizaje y de un perfil muy tecnico. Además, en mis ratos libres, me gusta disfrutar de la música, los amigos y el deporte.</p></li>
                         </ul>
@@ -152,13 +200,13 @@
                 <hr>
                 <footer>
                     <ul>
-                        <li><i class="fa fa-user fa-2x">Iker Ocio Zuazo</i>
+                        <li><i class="fa fa-user"><a href="http://ikerocio.com">Iker Ocio Zuazo</a></i>
                         </li>
-                        <li><i class="fa fa-linkedin-square fa-2x"><a href="https://www.linkedin.com/hp/?dnr=TGyOzgcX4qa3QuJirGRNGgjo4qaIQIhgxmnw">LinkedIn</a></i>
+                        <li><i class="fa fa-linkedin-square "><a href="https://www.linkedin.com/hp/?dnr=TGyOzgcX4qa3QuJirGRNGgjo4qaIQIhgxmnw">LinkedIn</a></i>
                         </li>
-                        <li><i class="fa fa-flickr fa-2x"><a href="https://www.flickr.com/photos/ikerocio/">FlickR</a></i>
+                        <li><i class="fa fa-flickr"><a href="https://www.flickr.com/photos/ikerocio/">FlickR</a></i>
                         </li>
-                        <li><i class="fa fa-envelope fa-2x"><a href="mailto:iker@ikerocio.com">iker@ikerocio.com</a></i>
+                        <li><i class="fa fa-envelope"><a href="mailto:iker@ikerocio.com">iker@ikerocio.com</a></i>
                     </ul>
 
                 </footer>
@@ -188,7 +236,5 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="js/jquery-1.11.2.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
 </body>
 </html>
