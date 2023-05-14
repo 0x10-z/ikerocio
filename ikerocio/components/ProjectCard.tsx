474f88f841d634dad3ce0a6d5262c3f69bc9e561
Iker Ocio <iker@ikerocio.com>
2023-05-14 11:22:42 +0200

    Improved

--- ikerocio/components/ProjectCard.tsx
+++ ikerocio/components/ProjectCard.tsx
@@ -5,6 +5,7 @@ import {
   Typography,
   Avatar,
 } from "@material-tailwind/react";
+import { useEffect, useState } from "react";
 
 interface ProjectCardProps {
   title: string;
@@ -22,16 +23,55 @@ export default function ProjectCard({
   const style = {
     backgroundImage: `url('${image}')`,
   };
+
+  const movementDelta = 5; // less quantity is more movement
+  const [parallaxData, setParallaxData] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e: any) => {
+    if (!e.currentTarget) return;
+    const cardHeader = e.currentTarget.getBoundingClientRect();
+    const xPos = ((e.clientX - cardHeader.left) / cardHeader.width) * 100;
+    const yPos = ((e.clientY - cardHeader.top) / cardHeader.height) * 100;
+    setParallaxData({ x: xPos, y: yPos });
+  };
+
+  useEffect(() => {
+    let animationFrameId: number;
+
+    const handleMouseMoveWithAnimationFrame = (e: any) => {
+      // Cancel the previous animation frame request
+      cancelAnimationFrame(animationFrameId);
+
+      // Request a new animation frame with the updated parallax data
+      animationFrameId = requestAnimationFrame(() => {
+        handleMouseMove(e);
+      });
+    };
+
+    document.addEventListener("mousemove", handleMouseMoveWithAnimationFrame);
+
+    return () => {
+      // Cancel the animation frame request and remove the event listener
+      cancelAnimationFrame(animationFrameId);
+      document.removeEventListener("mousemove", handleMouseMoveWithAnimationFrame);
+    };
+  }, []);
+  
   return (
     <a href={url} target="_blank">
       <Card
         shadow={false}
-        className="relative shadow-xl hover:transform hover:scale-110 hover:z-10 rounded grid h-80 w-full max-w-20 items-end justify-center overflow-hidden text-center">
+        className="relative shadow-xl transition-all duration-500 ease-in-out hover:transform hover:scale-110 hover:z-10 rounded grid h-80 w-full max-w-20 items-end justify-center overflow-hidden text-center">
         <CardHeader
           floated={false}
           shadow={false}
           color="transparent"
-          style={style}
+          style={{
+            backgroundPositionX: `${-parallaxData.x / movementDelta}px`,
+            backgroundPositionY: `${-parallaxData.y / movementDelta}px`,
+            backgroundImage: `url('${image}')`,
+          }}
+          onMouseMove={handleMouseMove}
           className={`absolute inset-0 m-0 h-full w-full rounded-none bg-cover bg-center`}>
           <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50" />
         </CardHeader>
